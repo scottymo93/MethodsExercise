27a6c0f92b3092d8edd603f216466534a3c56023
Scott Dyrness <scottdyrness@gmail.com>
2025-06-02 11:33:37 -0700

    completed math exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MethodsExercise
+﻿using System.Runtime.CompilerServices;
+
+namespace MethodsExercise
 {
     public class Program
     {
@@ -24,9 +26,52 @@
                               $" But then one day, {name} saw a huge {animal} {verb1} out from nowhere!" +
                               $" Terrified, {name} {verb2} off into the night. The End.");
         }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static int Subtract(int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+
+        public static int Multiply(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public static int Divide(int numOne, int numTwo)
+        {
+            return numOne / numTwo;
+        }
+
+        public static int Sum(params int[] Numbers)
+        {
+            int sum = 0;
+
+            foreach (int number in Numbers)
+            {
+                //sum = sum + number;
+                sum += number;
+            }
+            
+            return sum;
+        }
         static void Main(string[] args)
         {
-          GoodStory();
+          //GoodStory();
+
+          //Console.WriteLine(Add(2, 3));
+          
+          //Console.WriteLine(Subtract(3,2));
+          
+          //Console.WriteLine(Multiply(2,3));
+
+          //Console.WriteLine(Divide(10, 2));
+
+          //Console.WriteLine(Sum(5, 5, 5));
         }
     }
 }
